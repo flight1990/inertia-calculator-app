e6b798eb5369ec5e1f2eb55ec8a6af9097104d58
KRordel <romkakordel@gmail.com>
2024-07-04 16:53:16 +0300

    fix

--- resources/js/Components/Admin/Sidebar/SidebarMobileComponent.vue
+++ resources/js/Components/Admin/Sidebar/SidebarMobileComponent.vue
@@ -1,8 +1,12 @@
 <script setup>
-    import MenuComponent from "@/Components/Admin/Menu/MenuComponent.vue";
-
+    import {Link} from "@inertiajs/vue3";
     import ROfcanvas from "@/Components/Base/ROfcanvas.vue";
     import {DialogClose} from 'radix-vue';
+    import {usePage} from "@inertiajs/vue3";
+    import {computed} from "vue";
+
+    const page = usePage();
+    const menu = computed(() => page.props.menu);
 </script>
 <template>
     <ROfcanvas title="Меню" :width="320" side="left">
@@ -42,7 +46,16 @@
                 </button>
         </template>
         <template v-slot:body>
-            <MenuComponent />
+            <ul class="space-y-1.5">
+                <li v-for="item in menu.data" :key="item.id">
+                    <DialogClose as-child>
+                        <Link :href="item.url" class="w-full flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-neutral-700 rounded-lg hover:bg-gray-100"
+                            :class="{'bg-gray-100' : item.isActive}">
+                            {{ item.name }}
+                        </Link>
+                    </DialogClose>
+                </li>
+            </ul>
         </template>
     </ROfcanvas>
 </template>
--- resources/js/Pages/Admin/Index.vue
+++ resources/js/Pages/Admin/Index.vue
@@ -2,10 +2,56 @@
 
 import {Head} from "@inertiajs/vue3";
 import Layout from "@/Layouts/Admin/Admin.vue";
+import {reactive, ref} from "vue";
 
 defineOptions({
     layout: Layout
 });
+
+
+const usertotal = ref(0);
+const calctotal = ref(0);
+const seototal = ref(0);
+const categorytotal = ref(0);
+
+const fetchUsers = async () => {
+    try {
+        const {data} = await axios.get('/api/users');
+        usertotal.value = data.meta.total;    
+    } catch (e) {
+
+    } 
+}
+const fetchCalc = async () => {
+    try {
+        const {data} = await axios.get('/api/calculators');
+        calctotal.value = data.meta.total;    
+    } catch (e) {
+
+    } 
+}
+const fetchSeo = async () => {
+    try {
+        const {data} = await axios.get('/api/seo');
+        seototal.value = data.meta.total;    
+    } catch (e) {
+
+    } 
+}
+const fetchCategory = async () => {
+    try {
+        const {data} = await axios.get('/api/categories');
+        categorytotal.value = data.meta.total;    
+    } catch (e) {
+
+    } 
+}
+
+fetchUsers();
+fetchCalc();
+fetchSeo();
+fetchCategory();
+
 </script>
 <template>
     <Head>
@@ -13,6 +59,10 @@ defineOptions({
     </Head>
 
     <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
+
+        
+
+
         <div class="flex flex-col bg-white border shadow-sm rounded-xl">
             <div class="p-4 md:p-5">
                 <div class="flex items-center gap-x-2">
@@ -22,7 +72,7 @@ defineOptions({
                 </div>
                 <div class="mt-1 flex items-center gap-x-2">
                     <h3 class="text-xl sm:text-2xl font-medium text-gray-800">
-                        N
+                        {{usertotal}}
                     </h3>
                 </div>
             </div>
@@ -36,7 +86,7 @@ defineOptions({
                 </div>
                 <div class="mt-1 flex items-center gap-x-2">
                     <h3 class="text-xl sm:text-2xl font-medium text-gray-800">
-                        N
+                        {{calctotal}}
                     </h3>
                 </div>
             </div>
@@ -50,7 +100,7 @@ defineOptions({
                 </div>
                 <div class="mt-1 flex items-center gap-x-2">
                     <h3 class="text-xl sm:text-2xl font-medium text-gray-800">
-                        N
+                        {{seototal}}
                     </h3>
                 </div>
             </div>
@@ -64,7 +114,7 @@ defineOptions({
                 </div>
                 <div class="mt-1 flex items-center gap-x-2">
                     <h3 class="text-xl sm:text-2xl font-medium text-gray-800">
-                        N
+                        {{categorytotal}}
                     </h3>
                 </div>
             </div>
