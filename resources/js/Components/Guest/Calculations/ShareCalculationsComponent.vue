90fa80e5ef3398f5682c418dfa39dfd851978a70
KRordel <romkakordel@gmail.com>
2024-08-06 21:48:05 +0300

    share

--- resources/js/Components/Guest/Calculations/ShareCalculationsComponent.vue
+++ resources/js/Components/Guest/Calculations/ShareCalculationsComponent.vue
@@ -4,15 +4,21 @@
     import RDialog from "@/Components/Base/RDialog.vue";
     import {useUrlWatcher} from "@/Composables/useUrlWatcher.js";
 
+    import { SwitchRoot, SwitchThumb } from 'radix-vue'
+    import { ref } from 'vue'
+
+    const switchState = ref(false)
+
     const {url} = useUrlWatcher();
 
     const form = useForm({
         title: ''
     });
 
-    function handleClick() {
-        alert('hello!')
+    const copyText = () => {
+        alert("Copied text " + url.value);
     }
+
 </script>
 <template>
     <RDialog title="Поделиться" :width="420">
@@ -31,7 +37,34 @@
             </button>
         </template>
         <template v-slot:body>
-            {{ url }}
+            <div class="flex items-center gap-2">
+                <FTextInput v-model="url" class="w-full" />
+                <button type="button" @click="copyText"
+                    class="py-3 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none">
+                    Копировать
+                </button>
+            </div>
+            <div class="flex gap-2 items-center mt-2">
+                <SwitchRoot
+                    id="airplane-mode"
+                    v-model:checked="switchState"
+                    class="w-[34px] h-[18px] focus-within:!outline-none focus-within:outline-black flex bg-black/30 shadow-sm rounded-full relative data-[state=checked]:bg-primary-500 cursor-default"
+                >
+                    <SwitchThumb
+                        class="block w-[14px] h-[14px] my-auto bg-white shadow-sm rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[18px]"
+                    />
+                </SwitchRoot>
+                <label class="text-[15px] leading-none pr-[15px] select-none" for="airplane-mode">
+                    {{ switchState ? 'Поделиться расчетом' : 'Не делиться расчетом' }}
+                </label>
+            </div>
+            <div class="flex items-center justify-start mt-6">
+                <button type="button" @click="copyText"
+                    class="py-2 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none">
+                    <svg class="size-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
+                </button>
+            </div>
+            <!-- {{ url }} -->
         </template>
 
     </RDialog>
