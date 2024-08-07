c2633190604a75c69b99a6ee9048b212c292290d
KRordel <romkakordel@gmail.com>
2024-08-07 13:01:55 +0300

    share and main

--- resources/js/Components/Guest/Hero/HeroComponent.vue
+++ resources/js/Components/Guest/Hero/HeroComponent.vue
@@ -1,16 +1,14 @@
 <script setup>
-
-const {site_settings} = defineProps({
-    site_settings: Object
-});
-
+    const {site_settings} = defineProps({
+        site_settings: Object
+    });
 </script>
 
 <template>
-    <section id="hero" class="overflow-hidden py-8 sm:py-16 lg:pb-32">
+    <section id="hero" class="overflow-hidden">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div class="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
-                <div class="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
+            <div class="flex flex-col lg:flex-row items-center justify-between gap-8">
+                <div class="flex-1 lg:py-8">
                     <h1 class="text-3xl font-medium tracking-tight text-gray-700">
                         {{ site_settings.hero_title }}
                     </h1>
@@ -18,10 +16,8 @@ const {site_settings} = defineProps({
                         {{ site_settings.hero_description }}
                     </p>
                 </div>
-                <div class="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6 lg:ml-20">
-                    <div class="select-none -mx-4 h-[300px] px-10 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-8 xl:-bottom-32">
-                        <img src="/hero.png" class="select-none" alt="calcutor">
-                    </div>
+                <div class="order-first lg:order-last flex-1 flex items-center justify-center">
+                    <img src="/Calculator-bro.png" class="w-1/2 lg:w-full select-none pt-8 lg:p-20" alt="calcutor">
                 </div>
             </div>
         </div>
